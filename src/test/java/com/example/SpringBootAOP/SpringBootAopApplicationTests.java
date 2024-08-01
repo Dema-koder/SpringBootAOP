7373f26b3e515c6d87d08bf742d560c9fb98fbc3
Dema-koder <asotruzikovic@gmail.com>
2024-08-01 22:43:33 +0300

    Init commit

---
+++ src/test/java/com/example/SpringBootAOP/SpringBootAopApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.SpringBootAOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopApplicationTests {
+}
